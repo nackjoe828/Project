48d85d62a306a92c4e4502d9274f56dfe60c2945
chunfortam <tom19921004@hotmail.com>
2013-11-23 21:29:46 -0800

    added quary

--- Project/src/database/Model.java
+++ Project/src/database/Model.java
@@ -27,6 +27,7 @@ public class Model {
 	
 	public void getHistory() throws Exception{
 		result = connector.select("select * from history");
+		System.out.println("Pass");
 	}
 	
 	public void getVideo() throws Exception{
@@ -40,6 +41,23 @@ public class Model {
 	public void getChannel() throws Exception{
 		result = connector.select("select * from channel");
 	}
+	
+	public void getUserHistory(int uID) throws Exception
+	{
+		String quary = "SELECT h1.vID, h1.date	FROM History h1	WHERE h1.uID =" + Integer.toString(uID);
+		result = connector.select(quary);
+	}
+	
+	public void getLargestView() throws Exception
+	{
+		result = connector.select("SELECT vid FROM Video WHERE count >= all (SELECT count FROM Video)");
+	}
+	
+	public void getRecentHot() throws Exception
+	{
+		result = connector.select("SELECT vid, title FROM Video where count >= all(select count from video where date > now() - interval 7 day and date < now() ) and date > now() - interval 7 day and date < now() ");
+	}
+	
 	
 	public String getResult(){
 		return result.trim();
